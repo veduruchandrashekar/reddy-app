ea7c7a6995427e334b90cc0ca99524aa83c3b2c5
Chandrasekhar Reddy Veduru <102662246+veduruchandrashekar@users.noreply.github.com>
2023-05-22 14:31:37 +0100

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,22 @@
+variable "cidr_block" {
+    default = "10.20.0.0/16"
+}
+variable "instance_tenancy" {}
+
+
+variable "vpc_id" {}
+variable "subnet_id1" {}
+variable "subnet_id2" {}
+variable "subnet_cidr1" {
+    default = "10.20.1.0/24"
+}
+variable "subnet_cidr2" {
+  default = "10.20.2.0/24"
+}
+
+variable "sg_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [8080, 80, 22, 443]
+}
+
